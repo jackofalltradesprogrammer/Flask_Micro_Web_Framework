97a2881cbe8da9b43ff1ab548a375a51625d067f
Harpreet <jackofalltradesprogrammer@gmail.com>
2018-09-06 13:07:35 -0400

    Url building with 'url_for' by passing function name and arguments

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,10 +9,20 @@ def index():
 def hello():
 	return 'Hello, World!'
 
+@app.route('/login')
+def login():
+	return 'login'
+
 @app.route('/user/<username>')
-def show_user_profile(username):
+def profile(username):
 	#show the user profile for that user
-	return 'User %s' % username
+	return '{}\'s profile'.format(username)
+
+with app.test_request_context():
+	print(url_for('index'))
+	print(url_for('login'))
+	print(url_for('login', next='/'))
+	print(url_for('profile', username='John Doe'))
 
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
